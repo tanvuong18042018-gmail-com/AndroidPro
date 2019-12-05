281f4a0ca80b34e5fec1b946cb986709b36efac1
= <=>
2019-12-05 10:11:47 +0700

    Cap Nhat Bieu Do

--- Android/app/src/main/java/com/nhphuc/doen/QuestionActivity.java
+++ Android/app/src/main/java/com/nhphuc/doen/QuestionActivity.java
@@ -2,6 +2,7 @@ package com.nhphuc.doen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +10,7 @@ import android.widget.Button;
 
 public class QuestionActivity extends AppCompatActivity {
 
-    Button btnBuyCredit;
+    Button btnBuyCredit, btnAudience;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class QuestionActivity extends AppCompatActivity {
 
     public void AnhXa(){
         btnBuyCredit = (Button) findViewById(R.id.buttonBuyCredit);
+        btnAudience = (Button) findViewById(R.id.buttonAudience);
     }
 
     public void ShowActivity(){
@@ -32,10 +34,23 @@ public class QuestionActivity extends AppCompatActivity {
                 ActivityBuyCredit();
             }
         });
+
+        btnAudience.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupAudience();
+            }
+        });
     }
 
     public void ActivityBuyCredit(){
         Intent intent = new Intent(QuestionActivity.this, activity_credit.class);
         startActivity(intent);
+    }
+
+    public void PopupAudience(){
+        final Dialog dialogPopupConsultAudience = new Dialog(QuestionActivity.this);
+        dialogPopupConsultAudience.setContentView(R.layout.activity_popup_consult_audience);
+        dialogPopupConsultAudience.show();
     }
 }
